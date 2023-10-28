09ad22c9d9b95cde3d5a5d9994b77f6ce3f6c8e2
Denlich <muchalovych@gmail.com>
2023-10-28 14:13:13 +0200

    Add username in to Session Object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import authOptions from "@/auth/authOptions";
 import { Text } from "@radix-ui/themes";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
-  return <Text>Hello world!</Text>;
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  return <Text>Hello world! {session?.user.username}</Text>;
 }
---
+++ src/auth/authOptions.ts
@@ -0,0 +1,75 @@
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "../../prisma/client";
+import { NextAuthOptions } from "next-auth";
+import { compare } from "bcrypt";
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "john@email.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const existingUser = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!existingUser) return null;
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password!
+        );
+
+        if (!passwordMatch) return null;
+
+        return {
+          id: existingUser.id,
+          name: existingUser.name,
+          surname: existingUser.surname,
+          username: existingUser.username,
+          email: existingUser.email,
+        } as any;
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          username: user.username,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          username: token.username,
+        },
+      };
+    },
+  },
+};
+
+export default authOptions;
