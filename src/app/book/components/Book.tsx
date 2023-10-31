d23cedfa7cb33e1512013e0d1213cc4fbb6372ea
Denlich <muchalovych@gmail.com>
2023-10-31 22:48:57 +0100

    Add books for authors and publishers

--- src/app/book/components/Book.tsx
+++ src/app/book/components/Book.tsx
@@ -1,29 +1,36 @@
+"use client";
+
 import { Author, Book } from "@prisma/client";
 import { Box, Flex, IconButton, Text } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 interface BookProps {
   book: Book;
-  authors: Pick<Author, "name" | "surname" | "id">[];
+  authors?: Pick<Author, "name" | "surname" | "id">[];
 }
 
 const Book = ({ book, authors }: BookProps) => {
+  const router = useRouter();
+
   return (
     <Flex
       direction="column"
       className="bg-white rounded-xl p-3 hover:cursor-pointer hover:bg-gray-50"
       gap="3"
+      onClick={() => router.push(`/book/${book.id}`)}
     >
       <Box className="w-full h-64 bg-gray-200 rounded-xl" />
       <Flex direction="column">
         <Text className="text-lg font-semibold">{book.title}</Text>
         <Flex direction="column" className="space-y-1">
-          {authors.map((author) => (
-            <Text className="text-sm" key={author.id}>
-              {author.name} {author.surname}
-            </Text>
-          ))}
+          {authors &&
+            authors.map((author) => (
+              <Text className="text-sm" key={author.id}>
+                {author.name} {author.surname}
+              </Text>
+            ))}
         </Flex>
         <Text className="text-cya-500">${book.cost}</Text>
       </Flex>
